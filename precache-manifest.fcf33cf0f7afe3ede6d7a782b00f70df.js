fb9e07c5cedb145ea693cf3abaccc35448ba5c4b
Dan Chamorro <dan@danchamorro.com>
2019-02-12 16:56:24 -0500

    Updates

---
+++ precache-manifest.fcf33cf0f7afe3ede6d7a782b00f70df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f80f93aec132dbc0b2c",
+    "url": "/robofriends-redux/static/css/main.bc77b300.chunk.css"
+  },
+  {
+    "revision": "0f80f93aec132dbc0b2c",
+    "url": "/robofriends-redux/static/js/main.0f80f93a.chunk.js"
+  },
+  {
+    "revision": "a314a311352c0c28d69e",
+    "url": "/robofriends-redux/static/js/runtime~main.a314a311.js"
+  },
+  {
+    "revision": "426a16aea559281a9662",
+    "url": "/robofriends-redux/static/css/2.133223be.chunk.css"
+  },
+  {
+    "revision": "426a16aea559281a9662",
+    "url": "/robofriends-redux/static/js/2.426a16ae.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a0c2d30de61b68c18004f15f5db38997",
+    "url": "/robofriends-redux/index.html"
+  }
+];
